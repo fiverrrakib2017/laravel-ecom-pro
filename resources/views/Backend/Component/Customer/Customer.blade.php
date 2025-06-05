d4a4d65bd006664e88aa2507e6e640afe1ba96e4
Rakib Mahmud <fiverrrakib2017@gmail.com>
2025-06-05 17:46:27 +0600

    customer include processing..

--- resources/views/Backend/Component/Customer/Customer.blade.php
+++ resources/views/Backend/Component/Customer/Customer.blade.php
@@ -244,6 +244,18 @@
                 searchPlaceholder: 'Search...',
                 sSearch: '',
                 lengthMenu: '_MENU_ items/page',
+                 processing: `<div class="spinner-grow text-primary" role="status">
+                            <span class="sr-only">Loading...</span>
+                            </div>
+                            <div class="spinner-grow text-secondary" role="status">
+                            <span class="sr-only">Loading...</span>
+                            </div>
+                            <div class="spinner-grow text-success" role="status">
+                            <span class="sr-only">Loading...</span>
+                            </div>
+                            <div class="spinner-grow text-danger" role="status">
+                            <span class="sr-only">Loading...</span>
+                            </div>`,
             },
             "columns": [
                 // {
