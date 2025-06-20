b9f094652efb5af7ab1c66a69e1a14eb257e0832
Rakib Mahmud <fiverrrakib2017@gmail.com>
2025-06-20 19:06:53 +0600

    customer recharge list changes

--- resources/views/Backend/Pages/Customer/Credit/recharge_list.blade.php
+++ resources/views/Backend/Pages/Customer/Credit/recharge_list.blade.php
@@ -15,9 +15,8 @@
                        @php
     $branch_user_id = Auth::guard('admin')->user()->pop_id ?? null;
 
-    $dataQuery = App\Models\Customer_recharge::select('customer_id')
-        ->groupBy('customer_id')
-        ->latest();
+    $dataQuery = App\Models\Customer_recharge::select(DB::raw('MAX(id) as id'))
+    ->groupBy('customer_id');
 
     if ($branch_user_id) {
         $dataQuery->whereHas('customer', function ($query) use ($branch_user_id) {
