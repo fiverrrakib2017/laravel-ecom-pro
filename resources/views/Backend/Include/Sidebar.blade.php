36e14f19bf58576e35993c0c0401ebd7a0720428
Rakib@official <fiverrrakib2017@gmail.com>
2025-08-11 12:48:41 +0600

    settings permission completed

--- resources/views/Backend/Include/Sidebar.blade.php
+++ resources/views/Backend/Include/Sidebar.blade.php
@@ -963,32 +963,37 @@
                 @php
                     $active_prefix = ['admin.settings.information.index','admin.settings.passowrd.change.index'];
                 @endphp
-                <li class="nav-item">
-                    <a href="#"
-                        class="nav-link  {{ Str::startsWith($currentRoute, $active_prefix) ? 'active' : '' }}">
-                        <i class="fa fa-cog mr-2 "></i>
-                        <p>&nbsp; Settings <i class="right fas fa-angle-left"></i> </p>
-                    </a>
-                    <ul class="nav nav-treeview"
-                        style="{{ Str::startsWith($currentRoute, $active_prefix) ? 'display: block;' : 'display: none;' }}">
-
+                    @if(auth()->guard('admin')->user()->can('menu.access.settings'))
                         <li class="nav-item">
-                            <a href="{{ route('admin.settings.information.index') }}"
-                                class="nav-link {{ $route == 'admin.settings.information.index' ? 'active' : '' }}"><i
-                                    class="far fa-circle nav-icon"></i>
-                                <p>Application Information</p>
+                            <a href="#"
+                                class="nav-link  {{ Str::startsWith($currentRoute, $active_prefix) ? 'active' : '' }}">
+                                <i class="fa fa-cog mr-2 "></i>
+                                <p>&nbsp; Settings <i class="right fas fa-angle-left"></i> </p>
                             </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('admin.settings.passowrd.change.index') }}"
-                                class="nav-link {{ $route == 'admin.settings.passowrd.change.index' ? 'active' : '' }}"><i
-                                    class="far fa-circle nav-icon"></i>
-                                <p>Chagne Password</p>
-                            </a>
-                        </li>
+                            <ul class="nav nav-treeview"
+                                style="{{ Str::startsWith($currentRoute, $active_prefix) ? 'display: block;' : 'display: none;' }}">
+                                @if(auth()->guard('admin')->user()->can('settings.information.view'))
+                                <li class="nav-item">
+                                    <a href="{{ route('admin.settings.information.index') }}"
+                                        class="nav-link {{ $route == 'admin.settings.information.index' ? 'active' : '' }}"><i
+                                            class="far fa-circle nav-icon"></i>
+                                        <p>Application Information</p>
+                                    </a>
+                                </li>
+                                @endif
+                                @if(auth()->guard('admin')->user()->can('settings.password.change'))
+                                <li class="nav-item">
+                                    <a href="{{ route('admin.settings.passowrd.change.index') }}"
+                                        class="nav-link {{ $route == 'admin.settings.passowrd.change.index' ? 'active' : '' }}"><i
+                                            class="far fa-circle nav-icon"></i>
+                                        <p>Chagne Password</p>
+                                    </a>
+                                </li>
+                                @endif
 
-                    </ul>
-                </li>
+                            </ul>
+                        </li>
+                    @endif
                 @php
                     $active_prefix = ['admin.router'];
                 @endphp
