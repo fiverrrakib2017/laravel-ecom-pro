b52fecb6a3e3884cd26b88482e0dab7a1bc851f5
Rakib@official <fiverrrakib2017@gmail.com>
2025-09-03 12:10:52 +0600

    include app setings

--- resources/views/Portal/Support_and_tickets.blade.php
+++ resources/views/Portal/Support_and_tickets.blade.php
@@ -1,3 +1,9 @@
+ @php
+
+$app_settings= \App\Models\Website_information::first();
+
+@endphp
+
  <div class="card">
      <div class="card-header">
          <h3 class="card-title">Support & Quick Actions</h3>
@@ -7,15 +13,11 @@
          <a href="https://speed.cloudflare.com" target="_blank" class="btn btn-info btn-block mb-2"><i
                  class="fas fa-tachometer-alt mr-1"></i> Speed
              Test</a>
-         <button class="btn btn-warning btn-block mb-3" data-toggle="modal" data-target="#supportModal"><i
-                 class="fas fa-headset mr-1"></i> Create Support
-             Ticket</button>
+            <button type="button" class="btn btn-warning btn-block mb-3" data-toggle="modal" data-target="#supportModal"><i
+                 class="fas fa-headset mr-1"></i> Create Support Ticket</button>
          <div>
-             <p class="mb-1"><i class="fas fa-phone-alt mr-2"></i> 096xx-xxxxxx (24/7)</p>
-             <p class="mb-1"><i class="fab fa-whatsapp mr-2"></i> <a href="#">WhatsApp
-                     Chat</a></p>
-             <p class="mb-1"><i class="fab fa-facebook-messenger mr-2"></i> <a href="#">Messenger</a></p>
-             <p class="mb-0"><i class="far fa-envelope mr-2"></i> support@yourisp.com</p>
+             <p class="mb-1"><i class="fas fa-phone-alt mr-2"></i>{{$app_settings->phone_number ?? ''}} (24/7)</p>
+             <p class="mb-0"><i class="far fa-envelope mr-2"></i>{{$app_settings->email ?? ''}} </p>
          </div>
      </div>
  </div>
