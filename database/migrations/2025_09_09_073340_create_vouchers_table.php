a62f6766a72839cb96ec416282f6eed055043896
Rakib@official <fiverrrakib2017@gmail.com>
2025-09-09 07:35:57 +0600

    voucher create completed

---
+++ database/migrations/2025_09_09_073340_create_vouchers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('voucher_batch_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('router_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('hotspot_profile_id')->constrained()->cascadeOnDelete();
+            $table->string('username')->unique();
+            $table->text('password_encrypted');
+            $table->enum('status',['new','printed','sold','activated','expired','disabled'])->default('new');
+            $table->timestamp('activated_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->foreignId('hotspot_user_id')->nullable()->constrained()->nullOnDelete();
+            $table->unsignedInteger('use_count')->default(0);
+            $table->json('meta')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
