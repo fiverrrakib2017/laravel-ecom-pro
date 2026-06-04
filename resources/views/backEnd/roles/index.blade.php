79bc4db6efdca536bb29db287ab68b62f861d7ae
Rakib Mahmud <fiverrrakib2017@gmail.com>
2026-06-04 17:01:04 +0600

    role management show data

--- resources/views/backEnd/roles/index.blade.php
+++ resources/views/backEnd/roles/index.blade.php
@@ -1,87 +1,124 @@
 @extends('backEnd.layouts.master')
-@section('title','Roles Manage')
-
+@section('title','Role Management')
 @section('css')
-<link href="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-<link href="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-<link href="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-<link href="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-select-bs5/css/select.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-@endsection
 
+<style>
+
+</style>
+
+@endsection
 @section('content')
-<div class="container-fluid">
-    
-    <!-- start page title -->
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                <div class="page-title-right">
-                    <a href="{{route('users.create')}}" class="btn btn-primary rounded-pill">Create</a>
+ <div class="row">
+    <div class="col-12">
+        <div class="card border-0 shadow-sm mb-3">
+            <div class="card-body d-flex justify-content-between align-items-center flex-wrap">
+
+                <!-- Left Side -->
+                <div class="d-flex align-items-center">
+
+                    <div class="rounded-circle bg-primary bg-opacity-10 d-flex align-items-center justify-content-center me-3"
+                         style="width:60px;height:60px;">
+                        <i class="mdi mdi-account-group fs-2"></i>
+                    </div>
+
+                    <div>
+                        <h4 class="mb-1 ">Role Management</h4>
+                        <p class="text-muted mb-0">
+                            Manage user roles and permissions efficiently.
+                        </p>
+                    </div>
+
                 </div>
-                <h4 class="page-title">Roles Manage</h4>
+
+                <!-- Right Side -->
+                <div class="mt-3 mt-md-0">
+
+                    <a href="{{ route('roles.create') }}" class="btn btn-primary">
+                        <i class="mdi mdi-plus-circle-outline me-1"></i>
+                        Create New Role
+                    </a>
+
+                </div>
+
             </div>
         </div>
-    </div>       
-    <!-- end page title --> 
-   <div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-body">
-                <table id="datatable-buttons" class="table table-striped dt-responsive nowrap w-100">
-                    <thead>
-                        <tr>
-                            <th>SL</th>
-                            <th>Name</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                
-                
-                    <tbody>
-                        @foreach($show_data as $key=>$value)
-                        <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$value->name}}</td>
-                            <td>
-                                <div class="button-list">
-
-                                    <a href="{{route('roles.show',$value->id)}}" class="btn btn-xs btn-info waves-effect waves-light"><i class="fe-eye"></i></a>
-                                    <a href="{{route('roles.edit',$value->id)}}" class="btn btn-xs btn-primary waves-effect waves-light"><i class="fe-edit-1"></i></a>
-
-                                    <form method="post" action="{{route('roles.destroy')}}" class="d-inline">        
-                                        @csrf
-                                    <input type="hidden" value="{{$value->id}}" name="hidden_id">
-                                    <button type="submit" class="btn btn-xs btn-danger waves-effect waves-light delete-confirm"><i class="mdi mdi-close"></i></button></form>
-                                </div>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
- 
-            </div> <!-- end card body-->
-        </div> <!-- end card -->
-    </div><!-- end col-->
-   </div>
+    </div>
 </div>
+
+<div class="row">
+    <div class="col-12">
+        <div class="card shadow-sm border-0 rounded-3">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="_datatable" class="table table-hover align-middle mb-0">
+                        <thead >
+                            <tr>
+                                <th width="60">SL</th>
+                                <th>Name</th>
+
+                                <th width="160" class="text-center">Action</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            @foreach($show_data as $key => $value)
+                            <tr>
+                                <td>
+                                    <span class="fw-semibold text-muted">
+                                        {{ $loop->iteration }}
+                                    </span>
+                                </td>
+
+                                <td>
+                                    @if ($value->front_view == 1)
+                                        <span class="badge bg-dark px-3 py-2 rounded-pill">
+                                            {{ $value->name }}
+                                        </span>
+                                    @else
+                                        <span class="fw-medium">
+                                            {{ $value->name }}
+                                        </span>
+                                    @endif
+                                </td>
+
+
+
+
+
+                                 <td class="text-center">
+    <div class="d-flex justify-content-center gap-1">
+        <a href="{{route('roles.show',$value->id)}}" class="btn btn-info btn-sm">
+            <i class="fas fa-eye"></i>
+        </a>
+
+        <a href="{{route('roles.edit',$value->id)}}" class="btn btn-primary btn-sm">
+            <i class="fas fa-edit"></i>
+        </a>
+
+        <form method="POST" action="{{route('roles.destroy')}}">
+            @csrf
+            <input type="hidden" value="{{$value->id}}" name="hidden_id">
+
+            <button type="submit" class="btn btn-danger btn-sm delete-confirm">
+                <i class="fas fa-trash"></i>
+            </button>
+        </form>
+    </div>
+</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+   </div>
 @endsection
 
-
 @section('script')
-<!-- third party js -->
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-buttons/js/buttons.flash.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/datatables.net-select/js/dataTables.select.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/pdfmake/build/pdfmake.min.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/libs/pdfmake/build/vfs_fonts.js"></script>
-<script src="{{asset('/public/backEnd/')}}/assets/js/pages/datatables.init.js"></script>
-<!-- third party js ends -->
+ <script type="text/javascript">
+    $('#_datatable').DataTable();
+ </script>
+ {!! Toastr::message() !!}
 @endsection
