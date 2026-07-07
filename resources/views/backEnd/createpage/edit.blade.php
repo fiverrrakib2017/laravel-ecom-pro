851328e4c6d560fa2c5362de91f20d3f499e5ca0
Rakib Mahmud <fiverrrakib2017@gmail.com>
2026-07-07 13:14:12 +0600

    page update completed

--- resources/views/backEnd/createpage/edit.blade.php
+++ resources/views/backEnd/createpage/edit.blade.php
@@ -1,26 +1,49 @@
 @extends('backEnd.layouts.master')
 @section('title','Page Edit')
 @section('css')
-<link href="{{asset('public/backEnd')}}/assets/libs/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
-<link href="{{asset('public/backEnd')}}/assets/libs/summernote/summernote-lite.min.css" rel="stylesheet" type="text/css" />
+<style>
+
+</style>
+<link href="{{asset('backEnd')}}/assets/libs/summernote/summernote-lite.min.css" rel="stylesheet"/>
+
 @endsection
 @section('content')
 <div class="container-fluid">
-    
     <!-- start page title -->
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                <div class="page-title-right">
-                    <a href="{{route('pages.index')}}" class="btn btn-primary rounded-pill">Manage</a>
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm border-0 mb-3">
+                <div class="card-body d-flex justify-content-between align-items-center flex-wrap">
+
+                    <!-- Left -->
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="bg-success text-white rounded-circle d-flex align-items-center justify-content-center"
+                            style="width:50px; height:50px;">
+                            <i class="mdi mdi-file-document-outline fs-4"></i>
+                        </div>
+
+                        <div>
+                            <h4 class="mb-0">Page Edit</h4>
+                            <small class="text-muted"> Page Edit easily</small>
+                        </div>
+                    </div>
+
+                    <!-- Right -->
+                    <div class="d-flex align-items-center gap-3 mt-2 mt-sm-0">
+                        <div class="vr d-none d-sm-block"></div>
+
+                        <a href="{{ route('pages.index') }}" class="btn btn-primary">
+                            <i class="mdi mdi-file-document-outline me-1"></i> Manage Page
+                        </a>
+                    </div>
+
                 </div>
-                <h4 class="page-title">Page Edit</h4>
             </div>
         </div>
-    </div>       
-    <!-- end page title --> 
-   <div class="row">
-    <div class="col-lg-12">
+    </div>
+    <!-- end page title -->
+   <div class="row justify-content-center">
+    <div class="col-lg-8">
         <div class="card">
             <div class="card-body">
                 <form action="{{route('pages.update')}}" method="POST" class=row data-parsley-validate=""  enctype="multipart/form-data">
@@ -78,6 +101,7 @@
                     </div>
                     <!-- col end -->
                     <div>
+                         <button type="button" onclick="history.back();" class="btn btn-danger">Back</button>
                         <input type="submit" class="btn btn-success" value="Submit">
                     </div>
 
@@ -90,18 +114,16 @@
 </div>
 @endsection
 
-
-
 @section('script')
-<script src="{{asset('public/backEnd/')}}/assets/libs/parsleyjs/parsley.min.js"></script>
-<script src="{{asset('public/backEnd/')}}/assets/js/pages/form-validation.init.js"></script>
-<script src="{{asset('public/backEnd/')}}/assets/js/pages/form-advanced.init.js"></script>
 <!-- Plugins js -->
-<script src="{{asset('public/backEnd/')}}/assets/libs//summernote/summernote-lite.min.js"></script>
-<script>
-  $(".summernote").summernote({
-    placeholder: "Enter Your Text Here",
-    
-  });
-</script>
+<script src="{{asset('backEnd/')}}/assets/libs/summernote/summernote-lite.min.js"></script>
+ <script type="text/javascript">
+    $(document).ready(function () {
+        $('.summernote').summernote({
+            placeholder: "Enter Your Text Here",
+            height: 200,
+        });
+    });
+ </script>
+  {!! Toastr::message() !!}
 @endsection
